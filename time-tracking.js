bc59bfff0ecb66cde175ce1308502d0704ea1b94
Muralidhar22 <muralidhar7794@gmail.com>
2022-01-24 10:32:27 +0530

    Javascript modified

--- time-tracking.js
+++ time-tracking.js
@@ -5,29 +5,72 @@ const monthly = document.getElementById("monthly-btn")
 const hours = document.getElementsByClassName("hrs")
 const lastTime = document.getElementsByClassName("previous")
 
-Array.from(hours).forEach((e,i) => {
-        console.log(`${e}${i}`)
+    dailyData = () =>{
+        fetch("data.json")
+        .then(res => {
+            return res.json()
+        })
+        .then(loadedData => {
+           Array.from(hours).forEach((element, index) => {
+               hours[index].textContent =  `${loadedData[index].timeframes.daily.current}hrs`
+               lastTime[index].textContent = `Last Week - ${loadedData[index].timeframes.daily.previous}`
+           });
+        })
+        .catch(err => {
+            console.error(err)
+        })
+    }
+
+    weeklyData = () =>{
+        fetch("data.json")
+        .then(res => {
+            return res.json()
+        })
+        .then(loadedData => {
+            Array.from(hours).forEach((element, index) => {
+                hours[index].textContent =  `${loadedData[index].timeframes.weekly.current}hrs`
+                lastTime[index].textContent = `Last Week - ${loadedData[index].timeframes.weekly.previous}`
+            });
+        })
+        .catch(err => {
+            console.error(err)
+        })
+    }
+
+    monthlyData = () =>{
+        fetch("data.json")
+        .then(res => {
+            return res.json()
+        })
+        .then(loadedData => {
+            Array.from(hours).forEach((element, index) => {
+                hours[index].textContent =  `${loadedData[index].timeframes.monthly.current}hrs`
+                lastTime[index].textContent = `Last Week - ${loadedData[index].timeframes.monthly.previous}`
+            });
+        })
+        .catch(err => {
+            console.error(err)
+        })
+    }
+
+
+    daily.addEventListener('click', ()=> {
+        daily.classList.remove('text-muted')
+        weekly.classList.add('text-muted')
+        monthly.classList.add('text-muted')
+        dailyData()
     })
 
+    weekly.addEventListener('click', () => {
+        weekly.classList.remove('text-muted')
+        monthly.classList.add('text-muted')
+        daily.classList.add('text-muted')
+        weeklyData()
+    })
 
-
-daily.addEventListener('click', ()=> {
-    daily.classList.remove('text-muted')
-    weekly.classList.add('text-muted')
-    monthly.classList.add('text-muted')
-    dailyData()
-})
-
-weekly.addEventListener('click', () => {
-    weekly.classList.remove('text-muted')
-    monthly.classList.add('text-muted')
-    daily.classList.add('text-muted')
-    weeklyData()
-})
-
-monthly.addEventListener('click', () => {
-    monthly.classList.remove('text-muted')
-    weekly.classList.add('text-muted')
-    daily.classList.add('text-muted')
-    monthlyData()
-})
+    monthly.addEventListener('click', () => {
+        monthly.classList.remove('text-muted')
+        weekly.classList.add('text-muted')
+        daily.classList.add('text-muted')
+        monthlyData()
+    })
